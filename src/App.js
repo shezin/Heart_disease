28f0e303059d8c6855d2a958100bee7ac2b11690
shezin <shezinvenkitta@gmail.com>
2022-03-15 15:50:38 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.css';
 import TextField from '@mui/material/TextField';
 import Boxes  from './Components/Box/Box.js';
+import Nav from './Components/Nav/Nav';
+var perf = require('./Components/Box/Box.Html');
+
+
 
 
 function App() {
   return (
+    <div>
+
+    
+    <Nav/>
     <Boxes/>
+  
+    
+    </div>
+
+
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Nav.css";
+function Nav(){
+    return(
+    <div className="main-header">
+        <div className="nav-bar">
+            <div className="heading">Heart Disease Prediction</div>
+        </div>
+    </div>
+
+    );
+}
+export default Nav;
